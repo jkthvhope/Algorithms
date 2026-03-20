76546cc9ddb7351dfb500ad8ca18cfd52991ccd4
jkthvhope <vikakoza00@gmail.com>
2026-03-20 23:19:33 +1000

    готово

---
+++ 1 лаба/11.py
@@ -0,0 +1,18 @@
+height = [1,8,6,2,5,4,8,3,7]
+left = 0
+right = len(height) - 1
+square = 0
+while left < right:
+    width = right - left
+    if height[left] < height[right]:
+        h = height[left]
+    else:
+        h = height[right]
+    square_peremen = width * h
+    if square_peremen > square:
+        square = square_peremen
+    if height[left] < height[right]:
+        left += 1
+    else:
+        right -= 1
+print(square)

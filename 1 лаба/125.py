50a48ab7d3a1406ad6bbe2d598adeb817996f278
jkthvhope <vikakoza00@gmail.com>
2026-03-20 21:59:52 +1000

    готово

---
+++ 1 лаба/125.py
@@ -0,0 +1,4 @@
+def is_palindrome(s):
+    clean_s = "".join(char.lower() for char in s if char.isalnum())
+    return clean_s == clean_s[::-1]
+print(is_palindrome("A man, a plan, a canal: Panama"))

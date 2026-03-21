07248a54445b60e78f543bfbe723c21ae92fa60f
jkthvhope <vikakoza00@gmail.com>
2026-03-21 14:55:12 +1000

    готово

--- 1 лаба/125.py
+++ 1 лаба/125.py
@@ -4,3 +4,18 @@ def is_palindrome(s):
 print(is_palindrome("A man, a plan, a canal: Panama"))
 # print(is_palindrome("race a car"))
 # print(is_palindrome(" "))
+
+"""left = 0
+right = len(s) - 1
+is_palindrome = True
+while left < right:
+    while left < right and not s[left].isalnum():
+        left += 1
+    while left < right and not s[right].isalnum():
+        right -= 1
+    if s[left].lower() != s[right].lower():
+        is_palindrome = False
+        break
+    left += 1
+    right -= 1
+print(is_palindrome)"""
